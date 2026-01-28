556d230401bf381897a59b96d3d926fba8ff0b81
prafful <prraful@gmail.com>
2026-01-28 10:18:32 +0000

    04

---
+++ src/main/java/com/example/demo/entity/MarketEntity.java
@@ -0,0 +1,51 @@
+package com.example.demo.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="myMarket")
+public class MarketEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(nullable = false)
+    public String equity;
+
+    @Column
+    public long price;
+
+    public MarketEntity(Long id, String equity, long price) {
+        this.id = id;
+        this.equity = equity;
+        this.price = price;
+    }
+
+    public MarketEntity() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getEquity() {
+        return equity;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setEquity(String equity) {
+        this.equity = equity;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+}
---
+++ src/main/java/com/example/demo/repository/MarketJPARepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.MarketEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MarketJPARepository extends JpaRepository<MarketEntity, Long > {
+
+}
